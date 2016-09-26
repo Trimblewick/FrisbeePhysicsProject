dc2aa7672712faf7dd5b26fd74ec8a134bc9c7b8
Trimblewick <gustav.ekelund@hotmail.com>
2016-09-26 22:24:01 +0200

    added comment saying ass

--- Assets/Scripts/Frisbee.cs
+++ Assets/Scripts/Frisbee.cs
@@ -1,31 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Frisbee : MonoBehaviour {
+public class Frisbee : MonoBehaviour
+{
     public Vector3 velocity;//velocity and also direction
-    
+
+
+    private float time;             //elapsed time from start of the throw
+    private Vector3 startPosition;  //position at start of the throw
 
     private const float gravetyConstant = 9.82f;
-    
-    private int test;
-    private float time;
-    private Vector3 startPosition; //position at start of the throw
 
-	// Use this for initialization
-	void Start () {
-        this.test = 0;
+    private const float airDensity = 1.23f; //average airdensity at sealevel (kg/m^3)
+
+    // Use this for initialization
+    void Start()
+    {
         this.time = 0;
         this.startPosition = transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         float dt = Time.deltaTime;
         time += dt;
-        
+
         transform.position = startPosition + this.velocity * time + new Vector3(0, -gravetyConstant * time * time * 0.5f, 0);//position with gravety only
-        
-        transform.Rotate(new Vector3(0, 1, 0));//TEST: denna gör rotation med 1 grad per frame
-            
-	}
+
+        transform.Rotate(new Vector3(0, 90, 0) * dt);//TEST: denna gör rotation med 90 grader per sekund
+
+
+        //ass
+    }
 }
