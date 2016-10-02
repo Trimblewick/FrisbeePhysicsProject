ea8ca8c8c51369d009b09fa2043e98bc0d0c7c1a
Steve-roper <oskar.warnhag@gmail.com>
2016-10-03 01:35:37 +0200

    Added some basic phrisbee physics stuff

--- Assets/Scripts/Frisbee.cs
+++ Assets/Scripts/Frisbee.cs
@@ -3,14 +3,20 @@ using System.Collections;
 
 public class Frisbee : MonoBehaviour
 {
-    public Vector3 velocity;//velocity and also direction
-
+    public Vector3 velocity;        //velocity and also direction
+    public float AoA = 2f;          //Angle of attack
+    public float A0 = -4f;          //initial angle
 
     private float time;             //elapsed time from start of the throw
     private Vector3 startPosition;  //position at start of the throw
 
-    private const float gravetyConstant = 9.82f;
-
+    private const float mass = 0.175f;      //standard weight for frisbee according to USA ultimate
+    private const float area = 0.0568f;     //standard area                -  ||  -
+    private const float Cl_0 = 0.1f;        //Cl at angle 0
+    private const float Cd_0 = 0.08f;       //Cd at angle 0
+    private const float Cl_a = 1.4f;        //Cl dependent on angle
+    private const float Cd_a = 2.72f;       //Cd dependent on angle
+    private const float g = 9.82f;          //Gravitational constant
     private const float airDensity = 1.23f; //average airdensity at sealevel (kg/m^3)
 
     // Use this for initialization
@@ -25,8 +31,14 @@ public class Frisbee : MonoBehaviour
     {
         float dt = Time.deltaTime;
         time += dt;
+        float Cl = Cl_0 + Cl_a * (AoA * Mathf.PI / 180); // Cl = Cl0 + Cl(angle)
+        float Cd = Cd_0 + Cd_a * (Mathf.Pow((AoA - A0) * Mathf.PI / 180, 2));
+        float Fl = airDensity * area * Cl * this.velocity.sqrMagnitude * 0.5f;  //F = ½pACv^2
+        float Fd = airDensity * area * Cd * this.velocity.sqrMagnitude * 0.5f;  //  -||-
 
-        transform.position = startPosition + this.velocity * time + new Vector3(0, -gravetyConstant * time * time * 0.5f, 0);//position with gravety only
+        // transform.position = startPosition + this.velocity * time + new Vector3(0, -g * time * time * 0.5f, 0);//position with gravity only
+        this.velocity = this.velocity + new Vector3(-Fd * dt, (Fl - g) * dt, -Fd * dt); //change to being negative compared to current x and z speeds
+        transform.position = startPosition + this.velocity * dt;
 
         transform.Rotate(new Vector3(0, 100, 0) * dt);//TEST: denna gör rotation med 90 grader per sekund
 
@@ -35,3 +47,4 @@ public class Frisbee : MonoBehaviour
         //ass
     }
 }
+
